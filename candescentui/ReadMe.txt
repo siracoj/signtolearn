1301522c7dca5075f829181142c4f9336be8fcbd
siracoj <siraco.jared@gmail.com>
2014-02-19 12:20:22 -0500

    Reorganizing
    Deleted Candesent NUI, added some of it to the main solution, started
    trying to get hand contour data

--- candescentui/ReadMe.txt
+++
@@ -1,5 +0,0 @@
-For more information, please go to:
-
-
-Source: http://candescentnui.codeplex.com
-Blog: http://blog.candescent.ch
---
+++ signtolearn/HandAreaTest/AreaGrab.cs
@@ -0,0 +1,38 @@
+﻿//NUI Imports
+using CCT.NUI.Core;
+using CCT.NUI.KinectSDK;
+using CCT.NUI.HandTracking;
+
+//System Imports
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandAreaTest
+{
+    class AreaGrab
+    {
+
+        static void Main(string[] args)
+        {
+            IDataSourceFactory dataSourceFactory = new SDKDataSourceFactory();
+            var handDataSource = new HandDataSource(dataSourceFactory.CreateShapeDataSource(), new HandDataSourceSettings());
+
+            handDataSource.NewDataAvailable += new NewDataHandler<HandCollection>(handDataSource_NewDataAvailable);
+            handDataSource.Start();
+            
+        }
+
+        static void handDataSource_NewDataAvailable(HandCollection data)
+        {
+            for (int index = 0; index < data.Count; index++)
+            {
+                var hand = data.Hands[index];
+                IList<Point> points = hand.Contour.Points;
+                float x = points[0].X;
+            }
+        }
+    }
+}

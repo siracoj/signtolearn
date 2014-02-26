16c06629f410e70d1126c61fa950774109d04077
listerg <rdmqwerty@hotmail.com>
2014-02-26 09:33:38 -0500

    added some database functions

---
+++ signtolearn/DAL/Sign.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public static class Sign
+    {
+        private static String GetSQLConnectionString()
+        {
+            String DBUser = "sa";
+            String DBPass = "Clasic22";
+            throw new NotImplementedException();
+        }
+
+        public static SignInfo GetSign(String UserName, char Letter)
+        {
+            SqlConnection conn = new SqlConnection(GetSQLConnectionString());
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(String.Format("SELECT Letter, UserName, Percentage, NumFingers, ClosestPoint, Area from Sign where UserName = '{0}' and Letter = '{1}'", UserName, Letter), conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            Letter = Char.Parse(reader[0].ToString());
+            UserName = reader[1].ToString();
+            double Percentage = Double.Parse(reader[2].ToString());
+            int NumFingers = Int32.Parse(reader[3].ToString());
+            double ClosestPoint = Double.Parse(reader[4].ToString());
+            double Area = Double.Parse(reader[5].ToString());
+            conn.Close();
+            return new SignInfo(Letter, UserName, Percentage, NumFingers, ClosestPoint, Area);
+        }
+
+        public static void AddSign(char Letter, String UserName, double Percentage, int NumFingers, double ClosestPoint, double Area)
+        {
+            SqlConnection conn = new SqlConnection(GetSQLConnectionString());
+            SqlCommand cmd = new SqlCommand("INSERT into Sign (Letter, UserName, Percentage, NumFingers, ClosestPoint, Area) VALUES (@letter, @username, @percentage, @numfingers, @closestpoint, @area)", conn);
+            cmd.Parameters.AddWithValue("@letter", Letter);
+            cmd.Parameters.AddWithValue("@username", UserName);
+            cmd.Parameters.AddWithValue("@percentage", Percentage);
+            cmd.Parameters.AddWithValue("@numfingers", NumFingers);
+            cmd.Parameters.AddWithValue("@closestpoint", ClosestPoint);
+            cmd.Parameters.AddWithValue("@area", Area);
+            cmd.ExecuteScalar();
+            conn.Close();
+        }
+
+        public static void AddSign(SignInfo Input)
+        {
+            AddSign(Input.Letter, Input.UserName, Input.Percentage, Input.NumFingers, Input.ClosestPoint, Input.Area);
+        }
+    }
+
+    public class SignInfo
+    {
+        public SignInfo(char _Letter, String _UserName, double _Percentage, int _NumFingers, double _ClosestPoint, double _Area)
+        {
+            Letter = _Letter;
+            UserName = _UserName;
+            Percentage = _Percentage;
+            NumFingers = _NumFingers;
+            ClosestPoint = _ClosestPoint;
+            Area = _Area;
+        }
+
+        public char Letter
+        {
+            private set { }
+        }
+
+        public String UserName
+        {
+            private set { }
+        }
+
+        public double Percentage
+        {
+            private set { }
+        }
+
+        public int NumFingers
+        {
+            private set { }
+        }
+
+        public double ClosestPoint
+        {
+            private set { }
+        }
+
+        public double Area
+        {
+            private set { }
+        }
+    }
+}
---
+++ signtolearn/DAL/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public static class User
+    {
+        private static String GetSQLConnectionString()
+        {
+            String DBUser = "sa";
+            String DBPass = "Clasic22";
+            throw new NotImplementedException();
+        }
+
+        public static List<String> GetUserNames()
+        {
+            List<String> retVal = new List<String>();
+            SqlConnection Conn = new SqlConnection(GetSQLConnectionString());
+            Conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT UserName from User", Conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                retVal.Add(reader[0].ToString());
+            }
+
+            Conn.Close();
+            return retVal;
+        }
+
+        public static String GetFirstName(String UserName)
+        {
+            SqlConnection Conn = new SqlConnection(GetSQLConnectionString());
+            Conn.Open();
+            SqlCommand cmd = new SqlCommand(String.Format("SELECT FirstName from User WHHERE UserName = '{0}'", UserName), Conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            String retVal = reader[0].ToString();
+            Conn.Close();
+            return retVal;
+        }
+    }
+}
